8083239a9ae2d25ae409f3e549247c0044300442
slava <slava.tamashchuk@gmail.com>
2022-04-01 21:50:02 +0300

    color (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import pygame
 Width=950
 Height=950
 
+Red=0
+Green=0
+Blue=0
+
 program_onn=True
 
 print ("                               ")
@@ -28,7 +32,7 @@ sc.fill((255, 255, 255))
 def line_dragged(pos):
     global previus_pos
     if previus_pos is not None:
-        pygame.draw.line(sc, (0, 0, 0), previus_pos, pos, 5)
+        pygame.draw.line(sc, (Red, Green, Blue), previus_pos, pos, 5)
 
     previus_pos=pos
 
@@ -37,12 +41,53 @@ while program_onn:
         if e.type==pygame.QUIT:
             program_onn = False
             pygame.quit()
+        elif e.type==pygame.KEYDOWN:
+            if e.key==pygame.K_0:
+                Red=255
+                Green=255
+                Blue=255
+            elif e.key==pygame.K_1:
+                Red=0
+                Green=0
+                Blue=0
+            elif e.key==pygame.K_2:
+                Red=255
+                Green=0
+                Blue=0
+            elif e.key==pygame.K_3:
+                Red=255
+                Green=255
+                Blue=0
+            elif e.key==pygame.K_4:
+                Red=0
+                Green=255
+                Blue=0
+            elif e.key==pygame.K_5:
+                Red=0
+                Green=255
+                Blue=255
+            elif e.key==pygame.K_6:
+                Red=0
+                Green=190
+                Blue=255
+            elif e.key==pygame.K_7:
+                Red=0
+                Green=0
+                Blue=255
+            elif e.key==pygame.K_8:
+                Red=128
+                Green=0
+                Blue=128
+            elif e.key==pygame.K_9:
+                Red=255
+                Green=0
+                Blue=128
         elif e.type == pygame.MOUSEMOTION:
             if e.buttons == (1,0,0):
                 line_dragged(e.pos)
             else:
                 previus_pos = None
-    pygame.display.update()
+                pygame.display.update()
     pygame.time.delay(1)
 
 pygame.quit()
